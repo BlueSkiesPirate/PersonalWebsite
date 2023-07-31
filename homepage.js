20fd8e611e1c150eac348ff0dadf4f38bcd26ce1
BlueSkiesPirate <sebastianreyna128@gmail.com>
2023-07-30 20:01:38 -0400

    first commit

---
+++ homepage.js
@@ -0,0 +1,24 @@
+// const navOptions = document.querySelectorAll(".homepageNav1 .homepageNav2 button")
+// let chosen;
+// let restOfArray= [];
+// let ButtonsClicked =[];
+
+// Array.from(navOptions).forEach(choice => choice.addEventListener('click', (e) => {
+//     chosen = e.target.id
+
+// ButtonsClicked.push(chosen)
+
+// if(ButtonsClicked.length > 1  ){
+
+//     let chosenButton = document.getElementById(ButtonsClicked[1])
+    
+//     chosenButton.classList.add("selectedNav")
+
+//     let unchosenButton = document.getElementById(ButtonsClicked[0])
+
+// unchosenButton.classList.remove("selectedNav")
+//     ButtonsClicked.shift()
+// }
+
+// }))
+
